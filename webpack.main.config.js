b2b81b35298bd19c07c8e241120cc9b3b9fd1259
Kav Singh <flakisms@gmail.com>
2022-04-17 13:39:06 +0200

    move native node support to main webpack only

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -4,7 +4,26 @@ module.exports = {
   target: "electron-main",
   node: { __dirname: false },
   entry: "./src/main/index.ts",
-  module: { rules },
+  module: {
+    rules: [
+      ...rules,
+      // Add support for native node modules
+      {
+        // We're specifying native_modules in the test because the asset relocator loader generates a
+        // "fake" .node file which is really a cjs file.
+        test: /native_modules\/.+\.node$/,
+        use: "node-loader",
+      },
+      {
+        test: /\.(m?js|node)$/,
+        parser: { amd: false },
+        use: {
+          loader: "@vercel/webpack-asset-relocator-loader",
+          options: { outputAssetBase: "native_modules" },
+        },
+      },
+    ],
+  },
   resolve: { extensions: [...extensions, ".json"] },
   plugins,
 };
