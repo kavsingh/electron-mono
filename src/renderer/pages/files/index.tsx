7630b0a2bddd5f0a1ece59607d0caceaee0b92cc
Kav Singh <flakisms@gmail.com>
2022-06-04 19:12:39 +0200

    remove unused import

--- src/renderer/pages/files/index.tsx
+++ src/renderer/pages/files/index.tsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { useFileDrop } from "~/renderer/hooks/file";
 
