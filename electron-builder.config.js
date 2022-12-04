8bae99d27866abd5c8551b119c95fe6825019178
Kav Singh <flakisms@gmail.com>
2022-12-04 14:13:59 +0100

    use js for builder config

---
+++ electron-builder.config.js
@@ -0,0 +1,50 @@
+/**
+ * @type {import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configuration
+ */
+const config = {
+	appId: "com.ts-electron.app",
+	productName: "TSElectron",
+	directories: {
+		buildResources: "build",
+	},
+	files: ["dist/**"],
+	asarUnpack: "**/*.{node,dll}",
+	afterSign: "build/notarize.js",
+	win: {
+		executableName: "ts-electron",
+	},
+	nsis: {
+		artifactName: "${name}-${version}-setup.${ext}",
+		shortcutName: "${productName}",
+		uninstallDisplayName: "${productName}",
+		createDesktopShortcut: "always",
+	},
+	mac: {
+		entitlementsInherit: "build/entitlements.mac.plist",
+		extendInfo: [
+			"NSCameraUsageDescription: Application requests access to the device's camera.",
+			"NSMicrophoneUsageDescription: Application requests access to the device's microphone.",
+			"NSDocumentsFolderUsageDescription: Application requests access to the user's Documents folder.",
+			"NSDownloadsFolderUsageDescription: Application requests access to the user's Downloads folder.",
+		],
+	},
+	dmg: {
+		artifactName: "${name}-${version}.${ext}",
+	},
+	linux: {
+		target: ["AppImage", "snap", "deb"],
+		maintainer: "electronjs.org",
+		category: "Utility",
+	},
+	appImage: {
+		artifactName: "${name}-${version}.${ext}",
+	},
+	npmRebuild: false,
+	publish: {
+		provider: "generic",
+		url: "https://example.com/auto-updates",
+	},
+};
+
+module.exports = config;
