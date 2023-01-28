26a11b380da0e1c7cf9847884a43821dec73f0e5
Kav Singh <flakisms@gmail.com>
2023-01-28 17:48:38 +0800

    use common data mock

--- src/renderer/trpc/__mocks__/client.ts
+++ src/renderer/trpc/__mocks__/client.ts
@@ -1,17 +1,13 @@
 import { vi } from "vitest";
 
+import { createMockSystemInfo } from "~/common/__test-helpers__/mock-data-creators/system-info";
+
 import type { AppTRPCClient } from "../client";
 
 const mockClient: AppTRPCClient = {
 	systemInfo: {
 		// @ts-expect-error TODO: resolve type mismatch
-		query: vi.fn(() =>
-			Promise.resolve({
-				os: "OS",
-				freeMemory: BigInt(50),
-				totalMemory: BigInt(100),
-			}),
-		),
+		query: vi.fn(() => Promise.resolve(createMockSystemInfo())),
 	},
 	heartbeat: { subscribe: vi.fn(() => ({ unsubscribe: vi.fn() })) },
 };
