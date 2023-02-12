dd7acf5fbdcd45d77b7b00771675cd063762ed45
Kav Singh <flakisms@gmail.com>
2023-02-12 01:41:21 +0100

    minify with sourcemaps

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,16 +4,27 @@ import tsconfigPathsPlugin from "vite-tsconfig-paths";
 
 import type { UserConfig } from "vite";
 
-const define = { E2E: JSON.stringify(process.env["E2E"] === "true") };
+const isE2E = process.env["E2E"] === "true";
+
+const define = {
+	E2E: JSON.stringify(isE2E),
+} satisfies NonNullable<UserConfig["define"]>;
+
+const build = {
+	minify: isE2E && "esbuild",
+	sourcemap: !isE2E,
+} satisfies NonNullable<UserConfig["build"]>;
 
 export const nodeConfig: UserConfig = {
 	define,
+	build,
 	resolve: { conditions: ["node"] },
 	plugins: [tsconfigPathsPlugin(), externalizeDepsPlugin()],
 };
 
 export const rendererConfig: UserConfig = {
 	define,
+	build,
 	resolve: { conditions: ["browser"] },
 	plugins: [tsconfigPathsPlugin(), solidPlugin()],
 };
