0bcf656c08744df45122a464d4202535acc8b0d6
Kav Singh <flakisms@gmail.com>
2023-03-04 13:14:12 +0100

    cleanup prettier types, update zod

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,4 @@
+/** @type {import('prettier').Config} */
 module.exports = {
 	quoteProps: "consistent",
 	trailingComma: "all",
--- tools/format.ts
+++ tools/format.ts
@@ -2,11 +2,11 @@ import { format } from "prettier";
 
 import config from "../.prettierrc";
 
-import type { Options, Config } from "prettier";
+import type { Options } from "prettier";
 
 function prettifier(parser: NonNullable<Options["parser"]>) {
 	return function prettify(content: string) {
-		return format(content, { ...(config as Config), parser });
+		return format(content, { ...config, parser });
 	};
 }
 
