8df81d809234343b41b489e0c8dfc308942d5b1f
Kav Singh <flakisms@gmail.com>
2023-07-08 15:22:54 +0200

    break up trpc router

--- src/main/trpc/router.ts
+++ src/main/trpc/router.ts
@@ -1,63 +1,16 @@
-import { observable } from "@trpc/server/observable";
-import { BrowserWindow, dialog, nativeTheme } from "electron";
+import routesFiles from "./routes-files";
+import routesStatus from "./routes-status";
+import routesTheme from "./routes-theme";
+import { router } from "./trpc-server";
 
-import { themeSourceSchema } from "~/common/lib/theme";
-import { electronOpenDialogOptionsSchema } from "~/common/schema/electron";
-import { getSystemInfo } from "~/main/services/system-info";
-
-import { publicProcedure, router } from "./trpc-server";
-
-import type { ThemeSource } from "~/common/lib/theme";
-import type { AppEvent, AppEventBus } from "~/main/services/app-event-bus";
+import type { AppEventBus } from "~/main/services/app-event-bus";
 
 export function createAppRouter(eventBus: AppEventBus) {
 	return router({
-		systemInfo: publicProcedure.query(() => getSystemInfo()),
-
-		heartbeat: publicProcedure.subscription(() => {
-			type Payload = AppEvent<"app/heartbeatEvent">;
-
-			return observable<Payload>((emit) => {
-				function handler(payload: Payload) {
-					emit.next(payload);
-				}
-
-				eventBus.on("app/heartbeatEvent", handler);
-
-				return function unsubscribe() {
-					eventBus.off("app/heartbeatEvent", handler);
-				};
-			});
-		}),
-
-		themeSource: publicProcedure.query(
-			(): ThemeSource => nativeTheme.themeSource,
-		),
-
-		setThemeSource: publicProcedure
-			.input(themeSourceSchema)
-			.mutation(({ input }) => {
-				nativeTheme.themeSource = input;
-			}),
-
-		showOpenDialog: publicProcedure
-			.input(electronOpenDialogOptionsSchema)
-			.query(({ input }) => {
-				// TODO: determine requesting window somehow?
-				const focusedWindow = BrowserWindow.getAllWindows().find((win) =>
-					win.isFocused(),
-				);
-
-				if (!focusedWindow) throw new Error("No focused window");
-
-				return dialog.showOpenDialog(
-					focusedWindow,
-					// circumvent exactOptionalPropertyTypes conflict with
-					// upstream types
-					input as StripUndefined<typeof input>,
-				);
-			}),
-	});
+		...routesTheme(),
+		...routesStatus(eventBus),
+		...routesFiles(),
+	} as const);
 }
 
 export type AppRouter = ReturnType<typeof createAppRouter>;
---
+++ src/main/trpc/routes-files.ts
@@ -0,0 +1,27 @@
+import { BrowserWindow, dialog } from "electron";
+
+import { electronOpenDialogOptionsSchema } from "~/common/schema/electron";
+
+import { publicProcedure } from "./trpc-server";
+
+export default function routesFiles() {
+	return {
+		showOpenDialog: publicProcedure
+			.input(electronOpenDialogOptionsSchema)
+			.query(({ input }) => {
+				// TODO: determine requesting window somehow?
+				const focusedWindow = BrowserWindow.getAllWindows().find((win) =>
+					win.isFocused(),
+				);
+
+				if (!focusedWindow) throw new Error("No focused window");
+
+				return dialog.showOpenDialog(
+					focusedWindow,
+					// circumvent exactOptionalPropertyTypes conflict with
+					// upstream types
+					input as StripUndefined<typeof input>,
+				);
+			}),
+	} as const;
+}
