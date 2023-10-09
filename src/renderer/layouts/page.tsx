4bdffd690d733938104b96d5798c38580d978f93
Kav Singh <flakisms@gmail.com>
2023-10-09 20:28:09 +0200

    h2 for page header

--- src/renderer/layouts/page.tsx
+++ src/renderer/layouts/page.tsx
@@ -3,7 +3,7 @@ import type { ParentProps } from "solid-js";
 export function PageHeader(props: ParentProps) {
 	return (
 		<div class="sticky top-0 bg-white/60 p-4 pt-8 backdrop-blur-md dark:bg-neutral-900/60">
-			{props.children}
+			<h2>{props.children}</h2>
 		</div>
 	);
 }
