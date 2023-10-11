76a28493c9e9afb81373825e685107a0b6528409
Kav Singh <flakisms@gmail.com>
2023-10-11 20:20:20 +0200

    update deps

--- src/renderer/layouts/page.tsx
+++ src/renderer/layouts/page.tsx
@@ -2,7 +2,7 @@ import type { ParentProps } from "solid-js";
 
 export function PageHeader(props: ParentProps) {
 	return (
-		<div class="sticky top-0 bg-white/60 p-4 pt-8 backdrop-blur-md dark:bg-neutral-900/60">
+		<div class="sticky top-0 bg-white/60 p-4 pt-8 text-lg font-bold backdrop-blur-md dark:bg-neutral-900/60">
 			<h2>{props.children}</h2>
 		</div>
 	);
--- src/renderer/pages/system-info/system-info-list.tsx
+++ src/renderer/pages/system-info/system-info-list.tsx
@@ -11,8 +11,8 @@ export default function SystemInfoList() {
 				<ul class="m-0 flex list-none flex-col gap-2 p-0">
 					<For each={Object.entries(info)}>
 						{([key, val]) => (
-							<li class="flex gap-1 border-b border-b-neutral-800 pb-2 last:border-b-0 last:pb-0 dark:border-b-neutral-300">
-								<span class="after:content-[':']">{key}</span>
+							<li class="flex gap-2 border-b border-b-neutral-200 pb-2 last:border-b-0 last:pb-0 dark:border-b-neutral-700">
+								<span class="font-semibold text-neutral-500">{key}</span>
 								<span>{String(val)}</span>
 							</li>
 						)}
