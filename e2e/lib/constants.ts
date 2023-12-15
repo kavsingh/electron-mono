89f1c5f8f25111ce70cb4019219796621051609f
Kav Singh <flakisms@gmail.com>
2023-12-15 19:59:06 +0100

    fix dirname in e2e constants

--- e2e/lib/constants.ts
+++ e2e/lib/constants.ts
@@ -1,3 +1,6 @@
-import path from "path";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export const PROJECT_ROOT = path.resolve(__dirname, "../../");
