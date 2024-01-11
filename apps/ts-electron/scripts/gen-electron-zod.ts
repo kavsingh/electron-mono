6ef22179d4761c8b4558b16dc6095081ebbccb3e
Kav Singh <flakisms@gmail.com>
2024-01-11 23:17:29 +0100

    fix gen electron schema

--- apps/ts-electron/scripts/gen-electron-zod.ts
+++ apps/ts-electron/scripts/gen-electron-zod.ts
@@ -1,22 +1,28 @@
 import { readFile, writeFile } from "node:fs/promises";
 import path from "node:path";
-import { fileURLToPath, pathToFileURL } from "node:url";
+import { fileURLToPath } from "node:url";
 
 import { generate } from "ts-to-zod";
 
 import { formatTypescriptContent } from "./format";
 
+async function getElectronTypesPath() {
+	// eslint-disable-next-line @typescript-eslint/await-thenable
+	const electronEntryPath = await import.meta.resolve("electron");
+	const electronModulePath = path.parse(electronEntryPath).dir;
+
+	return path.join(electronModulePath, "electron.d.ts");
+}
+
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
-const typesPath = path.join(
-	__dirname,
-	"../node_modules/electron/electron.d.ts",
-);
 
 function nameFilter(name: string) {
+	// return true;
 	return /opendialogoptions/i.test(name) || /filefilter/i.test(name);
 }
 
 export default async function genElectronZod(outFile: string) {
+	const typesPath = await getElectronTypesPath();
 	const result = generate({
 		nameFilter,
 		sourceText: (await readFile(typesPath)).toString(),
@@ -30,8 +36,9 @@ export default async function genElectronZod(outFile: string) {
 }
 
 if (
-	process.argv[0] &&
-	pathToFileURL(import.meta.url).href === process.argv[0]
+	// bun has bun executable at argv[0], file path at argv[1]
+	process.argv[1] &&
+	fileURLToPath(import.meta.url) === process.argv[1]
 ) {
 	void genElectronZod(path.join(__dirname, "../src/common/schema/electron.ts"));
 }
