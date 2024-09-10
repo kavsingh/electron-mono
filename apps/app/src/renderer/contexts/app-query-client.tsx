358950844b716207f345491e378f54000d237837
Kav Singh <flakisms@gmail.com>
2024-09-10 20:40:19 +0200

    use trpc-react-query

--- apps/app/src/renderer/contexts/app-query-client.tsx
+++ apps/app/src/renderer/contexts/app-query-client.tsx
@@ -1,11 +1,23 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ipcLink } from "electron-trpc/renderer";
+import { SuperJSON } from "superjson";
+
+import { trpc } from "#renderer/trpc";
 
 import type { PropsWithChildren } from "react";
 
 export function AppQueryClientProvider(props: PropsWithChildren) {
+	const queryClient = new QueryClient();
+	const trpcClient = trpc.createClient({
+		links: [ipcLink()],
+		transformer: SuperJSON,
+	});
+
 	return (
-		<QueryClientProvider client={new QueryClient()}>
-			{props.children}
-		</QueryClientProvider>
+		<trpc.Provider client={trpcClient} queryClient={queryClient}>
+			<QueryClientProvider client={queryClient}>
+				{props.children}
+			</QueryClientProvider>
+		</trpc.Provider>
 	);
 }
