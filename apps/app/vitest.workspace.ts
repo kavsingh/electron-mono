7998a4d2915387d5af65130f76f14de65017606a
Kav Singh <flakisms@gmail.com>
2024-11-19 20:19:47 +0100

    drop intermediate test configs

--- apps/app/vitest.workspace.ts
+++ apps/app/vitest.workspace.ts
@@ -1,6 +1,33 @@
-import { defineWorkspace } from "vitest/config";
+import { defineProject, defineWorkspace, mergeConfig } from "vitest/config";
+
+import { nodeConfig, rendererConfig } from "./electron.vite.config";
 
 export default defineWorkspace([
-	{ extends: "./vitest.node.config.ts" },
-	{ extends: "./vitest.renderer.config.ts" },
+	mergeConfig(
+		nodeConfig,
+		defineProject({
+			test: {
+				name: "node",
+				clearMocks: true,
+				include: ["src/{main,common,preload}/**/*.{test,spec}.?(m|c)[jt]s?(x)"],
+				environment: "node",
+				setupFiles: ["./vitest.node.setup.ts"],
+			},
+		}),
+	),
+	mergeConfig(
+		rendererConfig,
+		defineProject({
+			// https://dev.to/mbarzeev/testing-a-solidjs-component-using-vitest-2h35
+			test: {
+				name: "renderer",
+				clearMocks: true,
+				include: ["src/renderer/**/*.{test,spec}.?(m|c)[jt]s?(x)"],
+				environment: "jsdom",
+				setupFiles: ["./vitest.renderer.setup.ts"],
+				testTransformMode: { web: ["/.[jt]sx?$/"] },
+				server: { deps: { inline: [/solid-js/, /@solidjs/] } },
+			},
+		}),
+	),
 ]);
