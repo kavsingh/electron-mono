0707c94b7fe5a955aecdbac1e9ac09663c582405
Kav Singh <flakisms@gmail.com>
2024-11-29 19:33:13 +0100

    void args

--- apps/app/src/renderer/pages/home/index.tsx
+++ apps/app/src/renderer/pages/home/index.tsx
@@ -1,4 +1,7 @@
+import { createResource } from "solid-js";
+
 import Page from "#renderer/layouts/page";
+import { tipc } from "#renderer/tipc";
 
 import SystemInfoCard from "./system-info-card";
 import SystemStatsCard from "./system-stats-card";
@@ -11,8 +14,23 @@ export default function Home() {
 				<div class="space-y-6">
 					<SystemInfoCard />
 					<SystemStatsCard />
+					<TIPC />
 				</div>
 			</Page.Content>
 		</>
+	);
+}
+
+function TIPC() {
+	const [ping] = createResource(() => tipc.invoke("ping"));
+	const [pingMessage] = createResource(() =>
+		tipc.invoke("pingMessage", "message"),
+	);
+
+	return (
+		<div class="space-y-2">
+			<div>{ping()}</div>
+			<div>{pingMessage()}</div>
+		</div>
 	);
 }
