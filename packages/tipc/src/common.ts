d7dde209d95bc16bffe810e9206a88c604314ebc
Kav Singh <flakisms@gmail.com>
2024-12-01 14:26:21 +0100

    add logging ability

--- packages/tipc/src/common.ts
+++ packages/tipc/src/common.ts
@@ -94,3 +94,11 @@ export type Serializer = {
 };
 
 export type Serialized<T> = T;
+
+export type Logger = {
+	debug: (...args: unknown[]) => unknown;
+	verbose: (...args: unknown[]) => unknown;
+	info: (...args: unknown[]) => unknown;
+	warn: (...args: unknown[]) => unknown;
+	error: (...args: unknown[]) => unknown;
+};
