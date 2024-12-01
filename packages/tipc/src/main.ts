9946229ec7f3df4bfbbf48816fa7e4a2d47f62d9
Kav Singh <flakisms@gmail.com>
2024-12-01 14:26:21 +0100

    add logging ability

--- packages/tipc/src/main.ts
+++ packages/tipc/src/main.ts
@@ -1,34 +1,38 @@
 import { defaultSerializer, scopedChannel } from "./common";
 
-import type { Serializer, TIPCDefinitions, TIPCMain } from "./common";
+import type { Logger, Serializer, TIPCDefinitions, TIPCMain } from "./common";
 import type { BrowserWindow, IpcMain } from "electron";
 
 export function createTIPCMain<TDefinitions extends TIPCDefinitions>(
 	ipcMain: IpcMain,
-	options?: { serializer?: Serializer | undefined },
+	options?: {
+		serializer?: Serializer | undefined;
+		logger?: Logger | undefined;
+	},
 ) {
 	const serializer = options?.serializer ?? defaultSerializer;
+	const logger = options?.logger;
 
 	const handleProxy = new Proxy(
 		{},
 		{
 			get: (_, channel) => {
+				const scoped = scopedChannel(`invoke/${channel as string}`);
+
 				return new Proxy(() => undefined, {
 					apply: (__, ___, [handler]: [(...args: unknown[]) => unknown]) => {
-						ipcMain.handle(
-							scopedChannel(`invoke/${channel as string}`),
-							async (event, arg) => {
-								const result = await handler(
-									event,
-									serializer.deserialize(arg),
-								);
+						logger?.debug("add handler", { scoped, handler });
 
-								return serializer.serialize(result);
-							},
-						);
+						ipcMain.handle(scoped, async (event, arg: unknown) => {
+							logger?.debug("handle", { scoped, arg });
+
+							const result = await handler(event, serializer.deserialize(arg));
+
+							return serializer.serialize(result);
+						});
 
 						return function removeHandler() {
-							ipcMain.removeHandler(channel as string);
+							ipcMain.removeHandler(scoped);
 						};
 					},
 				});
@@ -40,14 +44,17 @@ export function createTIPCMain<TDefinitions extends TIPCDefinitions>(
 		{},
 		{
 			get: (_, channel) => {
+				const scoped = scopedChannel(`eventsMain/${channel as string}`);
+
 				return new Proxy(() => undefined, {
 					apply: (__, ___, [windows, payload]: [BrowserWindow[], unknown]) => {
+						const serialized = serializer.serialize(payload);
+
+						logger?.debug("publish", { scoped, windows, serialized });
+
 						for (const win of windows) {
 							if (!win.isDestroyed()) {
-								win.webContents.send(
-									scopedChannel(`eventsMain/${channel as string}`),
-									serializer.serialize(payload),
-								);
+								win.webContents.send(scoped, serialized);
 							}
 						}
 					},
@@ -60,17 +67,20 @@ export function createTIPCMain<TDefinitions extends TIPCDefinitions>(
 		{},
 		{
 			get: (_, channel) => {
+				const scoped = scopedChannel(`eventsRenderer/${channel as string}`);
+
 				return new Proxy(() => undefined, {
 					apply: (__, ___, [handler]: [(...args: unknown[]) => unknown]) => {
 						function eventHandler(event: unknown, payload: unknown) {
+							logger?.debug("subscribe handler", { scoped, payload });
 							void handler(event, serializer.deserialize(payload));
 						}
 
-						const scoped = scopedChannel(`eventsRenderer/${channel as string}`);
-
+						logger?.debug("subscribe", { scoped, handler });
 						ipcMain.addListener(scoped, eventHandler);
 
 						return function unsubscribe() {
+							logger?.debug("unsubscribe", { scoped, handler });
 							ipcMain.removeListener(scoped, eventHandler);
 						};
 					},
