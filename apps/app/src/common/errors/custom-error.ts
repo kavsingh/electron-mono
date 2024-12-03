aff4fb4c7960530d9376118c43b0d2db873c70a8
Kav Singh <flakisms@gmail.com>
2024-12-03 21:58:53 +0100

    use result type to allow custom error serialization

---
+++ apps/app/src/common/errors/custom-error.ts
@@ -0,0 +1,48 @@
+export const CUSTOM_ERROR_CODES = ["CODE_A", "CODE_B"] as const;
+
+export default class CustomError extends Error {
+	override name = "CustomError";
+	code: CustomErrorCode;
+
+	constructor(code: CustomErrorCode, message: string) {
+		super(message);
+
+		this.code = code;
+	}
+}
+
+export function isSerialized(value: unknown): value is SerializedCustomError {
+	return (
+		!!value &&
+		typeof value === "object" &&
+		"__serialized__" in value &&
+		value.__serialized__ === "CustomError"
+	);
+}
+
+export function serialize(inst: CustomError): SerializedCustomError {
+	return {
+		__serialized__: "CustomError",
+		name: inst.name,
+		message: inst.message,
+		code: inst.code,
+		stack: inst.stack,
+		cause: inst.cause,
+	};
+}
+
+export function deserialize(serialized: SerializedCustomError) {
+	const error = new CustomError(serialized.code, serialized.message);
+
+	if (serialized.stack) error.stack = serialized.stack;
+	if (serialized.cause) error.cause = serialized.cause;
+
+	return error;
+}
+
+export type SerializedCustomError = Omit<CustomError, "stack"> & {
+	stack?: string | undefined;
+	__serialized__: "CustomError";
+};
+
+export type CustomErrorCode = (typeof CUSTOM_ERROR_CODES)[number];
--- apps/app/src/main/tipc/index.ts
+++ apps/app/src/main/tipc/index.ts
@@ -2,6 +2,8 @@ import { BrowserWindow, dialog, ipcMain, nativeTheme } from "electron";
 
 import { createTIPCMain } from "tipc/main";
 
+import CustomError from "#common/errors/custom-error";
+import { serializer } from "#common/tipc/serializer";
 import { getSystemInfo } from "#main/services/system-info";
 import { getSystemStats } from "#main/services/system-stats";
 
@@ -9,35 +11,59 @@ import type { AppTIPC } from "#common/tipc/defintion";
 import type { AppEventBus } from "#main/services/app-event-bus";
 import type { SystemStats } from "#main/services/system-stats";
 
-const tipc = createTIPCMain<AppTIPC>(ipcMain);
+const tipc = createTIPCMain<AppTIPC>(ipcMain, { serializer });
 
 export function setupIpc(eventBus: AppEventBus) {
 	const remove = [
-		tipc.showOpenDialog.handle((_, input) => {
+		tipc.showOpenDialog.handle(async (_, input) => {
 			const focusedWindow = BrowserWindow.getAllWindows().find((win) =>
 				win.isFocused(),
 			);
 
-			if (!focusedWindow) throw new Error("No focused window");
+			if (!focusedWindow) {
+				return { result: "error", error: new Error("No focused window") };
+			}
 
-			return dialog.showOpenDialog(
-				focusedWindow,
-				// circumvent exactOptionalPropertyTypes conflict with
-				// upstream types
-				input,
-			);
+			try {
+				return {
+					result: "ok",
+					value: await dialog.showOpenDialog(
+						focusedWindow,
+						// circumvent exactOptionalPropertyTypes conflict with
+						// upstream types
+						input,
+					),
+				};
+			} catch (reason) {
+				return { result: "error", error: new Error(String(reason)) };
+			}
 		}),
 
-		tipc.getSystemInfo.handle(() => getSystemInfo()),
+		tipc.getSystemInfo.handle(async () => {
+			return Math.random() > 0.5
+				? { result: "ok", value: await getSystemInfo() }
+				: {
+						result: "error",
+						error: new CustomError("CODE_A", "something happened"),
+					};
+		}),
 
-		tipc.getSystemStats.handle(() => getSystemStats()),
+		tipc.getSystemStats.handle(async () => {
+			try {
+				return { result: "ok", value: await getSystemStats() };
+			} catch (reason) {
+				return { result: "error", error: new Error(String(reason)) };
+			}
+		}),
 
-		tipc.getThemeSource.handle(() => nativeTheme.themeSource),
+		tipc.getThemeSource.handle(() => {
+			return { result: "ok", value: nativeTheme.themeSource };
+		}),
 
 		tipc.setThemeSource.handle((_, themeSource) => {
 			nativeTheme.themeSource = themeSource;
 
-			return nativeTheme.themeSource;
+			return { result: "ok", value: nativeTheme.themeSource };
 		}),
 	];
 
