c1fd0a8a7efde5e6d991218fdc10d3d3f61690ed
Kav Singh <flakisms@gmail.com>
2024-12-03 21:58:53 +0100

    use result type to allow custom error serialization

---
+++ apps/app/src/common/tipc/serializer.ts
@@ -0,0 +1,43 @@
+import CustomError, {
+	serialize as serializeCustomError,
+	deserialize as deserializeCustomError,
+	isSerialized as isSerializedCustomError,
+} from "#common/errors/custom-error";
+
+import type { Serializer } from "tipc";
+
+function serialize(value: unknown): unknown {
+	if (!value) return value;
+
+	if (value instanceof CustomError) return serializeCustomError(value);
+
+	if (Array.isArray(value)) return value.map((val: unknown) => serialize(val));
+
+	if (typeof value === "object") {
+		return Object.fromEntries(
+			Object.entries(value).map(([key, val]) => [key, serialize(val)]),
+		);
+	}
+
+	return value;
+}
+
+function deserialize(value: unknown): unknown {
+	if (!value) return value;
+
+	if (isSerializedCustomError(value)) return deserializeCustomError(value);
+
+	if (Array.isArray(value)) {
+		return value.map((val: unknown) => deserialize(val));
+	}
+
+	if (typeof value === "object") {
+		return Object.fromEntries(
+			Object.entries(value).map(([key, val]) => [key, deserialize(val)]),
+		);
+	}
+
+	return value;
+}
+
+export const serializer = { serialize, deserialize } as Serializer;
--- apps/app/src/renderer/tipc/index.ts
+++ apps/app/src/renderer/tipc/index.ts
@@ -1,5 +1,7 @@
 import { createTIPCRenderer } from "tipc/renderer";
 
+import { serializer } from "#common/tipc/serializer";
+
 import type { AppTIPC } from "#common/tipc/defintion";
 
-export const tipc = createTIPCRenderer<AppTIPC>();
+export const tipc = createTIPCRenderer<AppTIPC>({ serializer });
