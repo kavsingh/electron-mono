c18911d67044d46dacb673e6c6264a182bdcb8e7
Kav Singh <flakisms@gmail.com>
2024-12-04 01:37:36 +0100

    provide basic serializer

--- apps/app/src/common/tipc/serializer.ts
+++ apps/app/src/common/tipc/serializer.ts
@@ -1,45 +1,16 @@
+import { createSerializer, createValueSerializer } from "tipc";
+
 import CustomError, {
 	serialize as serializeCustomError,
 	deserialize as deserializeCustomError,
 	isSerialized as isSerializedCustomError,
 } from "#common/errors/custom-error";
 
-import type { Serializer } from "tipc";
+const customErrorSerialzer = createValueSerializer({
+	isDeserialized: (val) => val instanceof CustomError,
+	isSerialized: isSerializedCustomError,
+	serialize: serializeCustomError,
+	deserialize: deserializeCustomError,
+});
 
-function serialize(value: unknown): unknown {
-	if (!value) return value;
-
-	if (value instanceof CustomError) return serializeCustomError(value);
-
-	if (value instanceof Error) return Error;
-
-	if (Array.isArray(value)) return value.map((val: unknown) => serialize(val));
-
-	if (typeof value === "object") {
-		return Object.fromEntries(
-			Object.entries(value).map(([key, val]) => [key, serialize(val)]),
-		);
-	}
-
-	return value;
-}
-
-function deserialize(value: unknown): unknown {
-	if (!value) return value;
-
-	if (isSerializedCustomError(value)) return deserializeCustomError(value);
-
-	if (Array.isArray(value)) {
-		return value.map((val: unknown) => deserialize(val));
-	}
-
-	if (typeof value === "object") {
-		return Object.fromEntries(
-			Object.entries(value).map(([key, val]) => [key, deserialize(val)]),
-		);
-	}
-
-	return value;
-}
-
-export const serializer: Serializer = { serialize, deserialize };
+export const serializer = createSerializer([customErrorSerialzer]);
