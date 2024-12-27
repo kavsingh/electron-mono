805cd9262f9c2160453fbbef38250a10ebf4e8e0
Kav Singh <flakisms@gmail.com>
2024-12-27 21:54:39 +0100

    wip: test helpers to emit events

--- apps/app/src/renderer/__test-helpers__/tipc-mocked.ts
+++ apps/app/src/renderer/__test-helpers__/tipc-mocked.ts
@@ -1,3 +1,8 @@
+import {
+	applyTipcMocks,
+	emitTipcMainEvent,
+	mockTipcRenderer,
+} from "tipc/test/renderer";
 import { vi } from "vitest";
 
 import { createMockOpenDialogReturnValue } from "#common/__test-helpers__/mock-data-creators/electron";
@@ -7,14 +12,18 @@ import {
 } from "#common/__test-helpers__/mock-data-creators/system";
 
 import type { AppTIPC } from "#common/tipc/definition";
-import type { TIPCMockRenderer } from "tipc/test/renderer";
 
-export const mockedTipcRenderer: TIPCMockRenderer<AppTIPC> = {
-	getThemeSource: vi.fn(() => Promise.resolve("dark" as const)),
-	setThemeSource: vi.fn((source) => Promise.resolve(source)),
-	getSystemInfo: vi.fn(() => Promise.resolve(createMockSystemInfo())),
-	getSystemStats: vi.fn(() => Promise.resolve(createMockSystemStats())),
-	showOpenDialog: vi.fn(() => {
-		return Promise.resolve(createMockOpenDialogReturnValue());
-	}),
-};
+export const { namespace: tipcNamespace, api: tipcApi } =
+	mockTipcRenderer<AppTIPC>({
+		getThemeSource: vi.fn(() => Promise.resolve("dark" as const)),
+		setThemeSource: vi.fn((source) => Promise.resolve(source)),
+		getSystemInfo: vi.fn(() => Promise.resolve(createMockSystemInfo())),
+		getSystemStats: vi.fn(() => Promise.resolve(createMockSystemStats())),
+		showOpenDialog: vi.fn(() => {
+			return Promise.resolve(createMockOpenDialogReturnValue());
+		}),
+	});
+
+export const applyMocks = applyTipcMocks<AppTIPC>;
+
+export const emitMainEvent = emitTipcMainEvent<AppTIPC>;
--- packages/tipc/src/test-renderer.ts
+++ packages/tipc/src/test-renderer.ts
@@ -1,9 +1,9 @@
 import { TIPC_GLOBAL_NAMESPACE } from "./common";
 
-import type { TIPCDefinitions } from "./common";
+import type { TIPCDefinitions, TIPCOperation } from "./common";
 import type { TIPCResult } from "./internal";
 import type { TIPCApi } from "./preload";
-import type { IpcRendererEvent } from "electron";
+import type { IpcRenderer, IpcRendererEvent } from "electron";
 
 const fnMocks: Record<string, (...args: unknown[]) => unknown> = {};
 const eventHandlers: Record<
@@ -83,11 +83,44 @@ export function applyTipcMocks<TDefs extends TIPCDefinitions>(
 	}
 }
 
+export function emitTipcMainEvent<
+	TDefs extends TIPCDefinitions,
+	TChannel extends string = EmitableChannel<TDefs>,
+>(
+	channel: TChannel,
+	payload: TDefs[TChannel] extends { operation: "sendMain" }
+		? keyof TDefs[TChannel]["payload"] extends never
+			? // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+				void | undefined
+			: TDefs[TChannel]["payload"]
+		: never,
+	event?: IpcRendererEvent,
+) {
+	eventHandlers[channel]?.forEach((handler) => {
+		handler(event ?? createMockIpcRendererEvent(), payload);
+	});
+}
+
+const mockIpcRendererEventDefaults: IpcRendererEvent = {
+	ports: [],
+	sender: {} as IpcRenderer,
+	preventDefault: () => undefined,
+	defaultPrevented: false,
+};
+
+export function createMockIpcRendererEvent(
+	transform?: (defaults: IpcRendererEvent) => IpcRendererEvent,
+) {
+	return transform
+		? transform({ ...mockIpcRendererEventDefaults })
+		: { ...mockIpcRendererEventDefaults };
+}
+
 export type TIPCMockRenderer<
 	TDefs extends TIPCDefinitions,
-	TKeys extends keyof TDefs = MockableKeys<TDefs>,
+	TMockableKey extends keyof TDefs = MockableChannel<TDefs>,
 > = {
-	[TKey in TKeys]: TDefs[TKey] extends {
+	[TKey in TMockableKey]: TDefs[TKey] extends {
 		operation: "invokeQuery" | "invokeMutation";
 	}
 		? (arg: TDefs[TKey]["arg"]) => Promise<Awaited<TDefs[TKey]["response"]>>
@@ -96,14 +129,26 @@ export type TIPCMockRenderer<
 			: never;
 };
 
-type MockableKeys<TDefs extends TIPCDefinitions> = string &
-	Extract<
-		{ [K in keyof TDefs]: { key: K; op: TDefs[K]["operation"] } }[keyof TDefs],
-		{ op: "invokeQuery" | "invokeMutation" | "sendRenderer" }
-	>["key"];
+type MockableChannel<TDefs extends TIPCDefinitions> = ChannelForOperation<
+	TDefs,
+	"invokeQuery" | "invokeMutation" | "sendRenderer"
+>;
 
-type _EmitableKeys<TDefs extends TIPCDefinitions> = string &
+type EmitableChannel<TDefs extends TIPCDefinitions> = ChannelForOperation<
+	TDefs,
+	"sendMain"
+>;
+
+type ChannelForOperation<
+	TDefs extends TIPCDefinitions,
+	TOperation extends TIPCOperation["operation"],
+> = string &
 	Extract<
-		{ [K in keyof TDefs]: { key: K; op: TDefs[K]["operation"] } }[keyof TDefs],
-		{ op: "sendMain" }
-	>["key"];
+		{
+			[TChannel in keyof TDefs]: {
+				channel: TChannel;
+				operation: TDefs[TChannel]["operation"];
+			};
+		}[keyof TDefs],
+		{ operation: TOperation }
+	>["channel"];
