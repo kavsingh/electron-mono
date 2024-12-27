daad0018ceee3ab600aad201571d261a5397099a
Kav Singh <flakisms@gmail.com>
2024-12-27 22:15:47 +0100

    check serializers

---
+++ apps/app/src/renderer/pages/home/serializer-check.tsx
@@ -0,0 +1,45 @@
+import { createMutation } from "@tanstack/solid-query";
+import { createMemo, Show } from "solid-js";
+
+import CustomError from "#common/errors/custom-error";
+import Button from "#renderer/components/button";
+import Card from "#renderer/components/card";
+import { tipc } from "#renderer/tipc";
+
+export default function SerializerCheck() {
+	const customErrorMutation = createMutation(() => ({
+		mutationFn: () => tipc.throwCustomError.mutate(),
+	}));
+
+	const customErrorMessage = createMemo(() => {
+		if (!customErrorMutation.error) return undefined;
+
+		return customErrorMutation.error instanceof CustomError
+			? `${customErrorMutation.error.code}: ${customErrorMutation.error.message}`
+			: "unknown error";
+	});
+
+	return (
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Serializer Check</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<Button
+					onClick={() => {
+						customErrorMutation.mutate();
+					}}
+				>
+					Throw custom error
+				</Button>
+				<Show when={customErrorMessage()} keyed>
+					{(message) => {
+						return (
+							<div class="rounded-sm bg-red-600 p-4 text-white">{message}</div>
+						);
+					}}
+				</Show>
+			</Card.Content>
+		</Card.Root>
+	);
+}
