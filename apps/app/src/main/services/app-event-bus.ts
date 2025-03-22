6e99f9d662e38029ca7add9efde9b9b449c42cb0
Kav Singh <flakisms@gmail.com>
2025-03-22 21:04:19 +0100

    build: drop typed-emitter

--- apps/app/src/main/services/app-event-bus.ts
+++ apps/app/src/main/services/app-event-bus.ts
@@ -1,18 +1,17 @@
 import { EventEmitter } from "node:events";
 
 import type { SystemStats } from "./system-stats";
-import type TypedEmitter from "typed-emitter";
 
 export function createAppEventBus() {
-	return new EventEmitter() as TypedEmitter<AppEventMap>;
+	return new EventEmitter<{ systemStats: [SystemStats] }>();
 }
 
 export type AppEventBus = ReturnType<typeof createAppEventBus>;
 
 export type AppEventName = keyof AppEventMap;
 
-export type AppEvent<K extends AppEventName> = Parameters<AppEventMap[K]>[0];
+export type AppEvent<K extends AppEventName> = AppEventMap[K][0];
 
 type AppEventMap = {
-	systemStats: (payload: SystemStats) => void | Promise<void>;
+	systemStats: [SystemStats];
 };
