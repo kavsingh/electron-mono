c19e952ad39d99250a4d496146777b9acbc3fa1d
Kav Singh <flakisms@gmail.com>
2025-06-07 14:53:27 +0200

    build(deps): update deps

--- apps/app/vitest.config.ts
+++ apps/app/vitest.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, defineProject, mergeConfig } from "vitest/config";
+
+import { mainConfig, rendererConfig } from "./electron.vite.config.ts";
 
 export default defineConfig({
 	test: {
@@ -14,5 +16,33 @@ export default defineConfig({
 			],
 			reporter: "lcov",
 		},
+		projects: [
+			mergeConfig(
+				await mainConfig({ mode: "test", command: "build" }),
+				defineProject({
+					test: {
+						name: "node",
+						environment: "node",
+						include: [
+							"src/{main,common,preload}/**/*.{test,spec}.?(m|c)[tj]s?(x)",
+						],
+						setupFiles: ["./src/vitest.node.setup.ts"],
+					},
+				}),
+			),
+			mergeConfig(
+				await rendererConfig({ mode: "test", command: "build" }),
+				defineProject({
+					resolve: { conditions: ["development", "browser"] },
+					test: {
+						name: "renderer",
+						environment: "jsdom",
+						include: ["src/renderer/**/*.{test,spec}.?(m|c)[tj]s?(x)"],
+						setupFiles: ["./src/vitest.renderer.setup.ts"],
+						server: { deps: { inline: [/solid-js/] } },
+					},
+				}),
+			),
+		],
 	},
 });
