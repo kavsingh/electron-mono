608081e4f45441292e1ed0407b829931147c19a3
Kav Singh <flakisms@gmail.com>
2025-11-02 23:03:31 +0100

    fix(app): build extend info config

--- apps/app/electron-builder.config.ts
+++ apps/app/electron-builder.config.ts
@@ -2,33 +2,30 @@ import type { Configuration } from "electron-builder";
 
 const config: Configuration = {
 	appId: "com.app.dev",
-	productName: "App",
 	directories: {
 		buildResources: "build",
 	},
 	files: ["out/**"],
-	asar: true,
-	asarUnpack: "**/*.{node,dll}",
-	win: {
-		executableName: "App",
-	},
 	nsis: {
-		artifactName: "${name}-${version}-setup.${ext}",
+		artifactName: "${productName}-${version}-setup.${ext}",
 		shortcutName: "${productName}",
 		uninstallDisplayName: "${productName}",
 		createDesktopShortcut: "always",
 	},
 	mac: {
 		target: { target: "default", arch: ["universal"] },
-		type: "distribution",
-		hardenedRuntime: true,
+		entitlements: "build/entitlements.mac.plist",
 		entitlementsInherit: "build/entitlements.mac.plist",
-		extendInfo: [
-			"NSCameraUsageDescription: Application requests access to the device's camera.",
-			"NSMicrophoneUsageDescription: Application requests access to the device's microphone.",
-			"NSDocumentsFolderUsageDescription: Application requests access to the user's Documents folder.",
-			"NSDownloadsFolderUsageDescription: Application requests access to the user's Downloads folder.",
-		],
+		extendInfo: {
+			NSCameraUsageDescription:
+				"Application requests access to the device's camera.",
+			NSMicrophoneUsageDescription:
+				"Application requests access to the device's microphone.",
+			NSDocumentsFolderUsageDescription:
+				"Application requests access to the user's Documents folder.",
+			NSDownloadsFolderUsageDescription:
+				"Application requests access to the user's Downloads folder.",
+		},
 	},
 	dmg: {
 		artifactName: "${name}-${version}-${arch}.${ext}",
