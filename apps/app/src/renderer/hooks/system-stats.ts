a7596e3b53567fe75ae2f8b353a9a3f672281ae9
Kav Singh <flakisms@gmail.com>
2025-12-09 07:30:49 +0100

    react + trpc via react-query

--- apps/app/src/renderer/hooks/system-stats.ts
+++ apps/app/src/renderer/hooks/system-stats.ts
@@ -1,59 +1,24 @@
-import { queryOptions, useQuery, useQueryClient } from "@tanstack/solid-query";
-import { Unsubscribable } from "@trpc/server/observable";
-import { scope } from "electron-log/renderer";
-
-import { SystemStats } from "~/common/schema/system";
 import { trpc } from "~/renderer/trpc";
 
-import type { QueryClient } from "@tanstack/solid-query";
-
-function systemStatsQuery() {
-	return queryOptions({
-		queryKey: ["systemStats"],
-		queryFn: () => trpc.systemStats.query(),
-		reconcile: (oldData, newData) => {
-			return oldData && BigInt(oldData.sampledAt) > BigInt(newData.sampledAt)
-				? oldData
-				: newData;
-		},
-	});
-}
-
-const startSubscription = (() => {
-	const logger = scope("system stats subscription");
-	const queryKey = systemStatsQuery().queryKey;
-	let cachedClient: QueryClient;
-	let unsubscribable: Unsubscribable | undefined = undefined;
-
-	return function start(queryClient: QueryClient) {
-		if (cachedClient === queryClient) return;
-
-		unsubscribable?.unsubscribe();
-		cachedClient = queryClient;
-
-		unsubscribable = trpc.systemStatsEvent.subscribe(undefined, {
-			onData(event) {
-				const current = cachedClient.getQueryData<SystemStats>(queryKey);
-				const shouldUpdate = current
-					? BigInt(event.sampledAt) >= BigInt(current.sampledAt)
-					: true;
-
-				if (shouldUpdate) {
-					cachedClient.setQueryData(queryKey, () => event);
-				}
-			},
-			onError(err) {
-				logger.error("systemStatsEvent subscription error:", err);
-			},
-		});
-	};
-})();
+// type reference portability
+import type { UseTRPCQueryResult as _ } from "@trpc/react-query/shared";
 
 export function useSystemStats() {
-	const queryClient = useQueryClient();
-	const query = useQuery(systemStatsQuery);
+	const utils = trpc.useUtils();
+	const query = trpc.systemStats.useQuery();
 
-	startSubscription(queryClient);
+	trpc.systemStatsEvent.useSubscription(undefined, {
+		onData(event) {
+			const current = utils.systemStats.getData();
+			const shouldUpdate = current
+				? BigInt(event.sampledAt) >= BigInt(current.sampledAt)
+				: true;
+
+			if (shouldUpdate) {
+				utils.systemStats.setData(undefined, () => event);
+			}
+		},
+	});
 
 	return query;
 }
