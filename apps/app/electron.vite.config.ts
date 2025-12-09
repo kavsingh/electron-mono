013910f437b59b1537071eab73fab9a48682e124
Kav Singh <flakisms@gmail.com>
2025-12-09 07:05:07 +0100

    build(deps): update deps

--- apps/app/electron.vite.config.ts
+++ apps/app/electron.vite.config.ts
@@ -2,11 +2,11 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 import tailwindcssPlugin from "@tailwindcss/vite";
-import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import { defineConfig } from "electron-vite";
 import solidPlugin from "vite-plugin-solid";
 import tsconfigPathsPlugin from "vite-tsconfig-paths";
 
-import type { UserConfig, UserConfigFn } from "vite";
+import type { UserConfig } from "vite";
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -14,59 +14,43 @@ const define = {
 	E2E: JSON.stringify(process.env["E2E"] === "true"),
 } satisfies NonNullable<UserConfig["define"]>;
 
-function build(mode: string): NonNullable<UserConfig["build"]> {
-	return mode === "production"
-		? { minify: "esbuild", sourcemap: true }
-		: { minify: false, sourcemap: false };
-}
+export default defineConfig(({ mode }) => {
+	const build =
+		mode === "production"
+			? { minify: "esbuild" as const, sourcemap: true }
+			: { minify: false, sourcemap: false };
 
-export const mainConfig: UserConfigFn = ({ mode }) => {
 	return {
-		define,
-		build: build(mode),
-		resolve: { conditions: ["node"] },
-		plugins: [
-			tsconfigPathsPlugin(),
-			externalizeDepsPlugin({ exclude: ["trpc-electron"] }),
-		],
-	};
-};
-
-const preloadConfig: UserConfigFn = ({ mode }) => {
-	return {
-		define,
-		plugins: [
-			tsconfigPathsPlugin(),
-			externalizeDepsPlugin({ exclude: ["trpc-electron"] }),
-		],
-		build: {
-			...build(mode),
-			rollupOptions: {
-				input: {
-					renderer: path.resolve(dirname, "src/preload/renderer.ts"),
-				},
-				output: {
-					format: "cjs",
-					entryFileNames: "[name].cjs",
-					chunkFileNames: "[name].cjs",
-					assetFileNames: "[name].[ext]",
+		main: {
+			define,
+			build: { ...build, externalizeDeps: { exclude: ["trpc-electron"] } },
+			resolve: { conditions: ["node"] },
+			plugins: [tsconfigPathsPlugin()],
+		},
+		preload: {
+			define,
+			build: {
+				...build,
+				externalizeDeps: { exclude: ["trpc-electron"] },
+				rollupOptions: {
+					input: {
+						renderer: path.resolve(dirname, "src/preload/renderer.ts"),
+					},
+					output: {
+						format: "cjs",
+						entryFileNames: "[name].cjs",
+						chunkFileNames: "[name].cjs",
+						assetFileNames: "[name].[ext]",
+					},
 				},
 			},
+			plugins: [tsconfigPathsPlugin()],
+		},
+		renderer: {
+			define,
+			build,
+			resolve: { conditions: ["browser", mode] },
+			plugins: [tsconfigPathsPlugin(), solidPlugin(), tailwindcssPlugin()],
 		},
 	};
-};
-
-export const rendererConfig: UserConfigFn = ({ mode }) => {
-	return {
-		define,
-		build: build(mode),
-		resolve: { conditions: ["browser", mode] },
-		plugins: [tsconfigPathsPlugin(), solidPlugin(), tailwindcssPlugin()],
-	};
-};
-
-export default defineConfig({
-	main: mainConfig,
-	preload: preloadConfig,
-	renderer: rendererConfig,
 });
--- apps/app/vitest.config.ts
+++ apps/app/vitest.config.ts
@@ -1,48 +1,52 @@
 import { defineConfig, defineProject, mergeConfig } from "vitest/config";
 
-import { mainConfig, rendererConfig } from "./electron.vite.config.ts";
+import defineBaseConfig from "./electron.vite.config.ts";
 
-export default defineConfig({
-	test: {
-		clearMocks: true,
-		coverage: {
-			include: [
-				"src",
-				"!**/__generated__",
-				"!**/__mocks__",
-				"!**/__{test,spec}*__",
-				"!**/{test,spec}.*",
-				"!**/types.*",
+export default defineConfig((configEnv) => {
+	const baseConfig = defineBaseConfig(configEnv);
+
+	return {
+		test: {
+			clearMocks: true,
+			coverage: {
+				include: [
+					"src",
+					"!**/__generated__",
+					"!**/__mocks__",
+					"!**/__{test,spec}*__",
+					"!**/{test,spec}.*",
+					"!**/types.*",
+				],
+				reporter: "lcov",
+			},
+			projects: [
+				mergeConfig(
+					baseConfig.main as Record<string, unknown>,
+					defineProject({
+						test: {
+							name: "node",
+							environment: "node",
+							include: [
+								"src/{main,common,preload}/**/*.{test,spec}.?(m|c)[tj]s?(x)",
+							],
+							setupFiles: ["./src/vitest.node.setup.ts"],
+						},
+					}),
+				),
+				mergeConfig(
+					baseConfig.renderer as Record<string, unknown>,
+					defineProject({
+						resolve: { conditions: ["development", "browser"] },
+						test: {
+							name: "renderer",
+							environment: "jsdom",
+							include: ["src/renderer/**/*.{test,spec}.?(m|c)[tj]s?(x)"],
+							setupFiles: ["./src/vitest.renderer.setup.ts"],
+							server: { deps: { inline: [/solid-js/] } },
+						},
+					}),
+				),
 			],
-			reporter: "lcov",
 		},
-		projects: [
-			mergeConfig(
-				await mainConfig({ mode: "test", command: "build" }),
-				defineProject({
-					test: {
-						name: "node",
-						environment: "node",
-						include: [
-							"src/{main,common,preload}/**/*.{test,spec}.?(m|c)[tj]s?(x)",
-						],
-						setupFiles: ["./src/vitest.node.setup.ts"],
-					},
-				}),
-			),
-			mergeConfig(
-				await rendererConfig({ mode: "test", command: "build" }),
-				defineProject({
-					resolve: { conditions: ["development", "browser"] },
-					test: {
-						name: "renderer",
-						environment: "jsdom",
-						include: ["src/renderer/**/*.{test,spec}.?(m|c)[tj]s?(x)"],
-						setupFiles: ["./src/vitest.renderer.setup.ts"],
-						server: { deps: { inline: [/solid-js/] } },
-					},
-				}),
-			),
-		],
-	},
+	};
 });
