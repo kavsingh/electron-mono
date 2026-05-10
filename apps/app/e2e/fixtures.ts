23f8b829595649591f3547819d2805308577b90c
Kav Singh <flakisms@gmail.com>
2026-05-11 01:52:32 +0200

    chore(repo): bump deps

--- apps/app/e2e/fixtures.ts
+++ apps/app/e2e/fixtures.ts
@@ -8,11 +8,24 @@ interface Fixtures {
 	app: ElectronApplication;
 }
 
+interface Options {
+	appLaunchOptions?:
+		| Partial<Parameters<typeof _electron.launch>[0]>
+		| undefined;
+}
+
 const appPath = path.resolve(import.meta.dirname, "../out/main/index.cjs");
 
-export const test = baseTest.extend<Fixtures>({
-	async app({ launchOptions: _ }, use) {
-		const app = await _electron.launch({ args: [appPath, "--e2e"] });
+export const test = baseTest.extend<Fixtures & Options>({
+	appLaunchOptions: [{}, { option: true }],
+
+	async app({ appLaunchOptions }, use) {
+		const { args, ...launchOptions } = appLaunchOptions ?? {};
+		const app = await _electron.launch({
+			args: [appPath, "--e2e", ...(args ?? [])],
+			colorScheme: "dark",
+			...launchOptions,
+		});
 
 		await use(app);
 		await app.close();
